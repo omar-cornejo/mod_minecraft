a16e0981bd997bc14d88b17be9415812e7bea709
OmarCornejo <omarcornejovargas@gmail.com>
2024-07-25 19:51:24 +0200

    archivos_iniciales
    Archivos iniciales para hacer mods en Minecraft

---
+++ src/client/java/vaper/ruben/VaperrubenClient.java
@@ -0,0 +1,10 @@
+package vaper.ruben;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VaperrubenClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/vaper/ruben/VaperrubenDataGenerator.java
@@ -0,0 +1,11 @@
+package vaper.ruben;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class VaperrubenDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
